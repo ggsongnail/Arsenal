54675dcc28a5e4557d5c4eebe839f6fb5d9455c2
Huangls <343851557@qq.com>
2018-12-24 18:28:25 +0800

    更换为父子moudle结构

--- bullet/src/main/java/com/arms/bullet/HttpClientShoot.java
+++ bullet/src/main/java/com/arms/bullet/HttpClientShoot.java
@@ -1,5 +1,6 @@
 package com.arms.bullet;
 
+import com.arms.ak47.pojo.User;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -9,6 +10,7 @@ import java.io.IOException;
 
 public class HttpClientShoot {
     public static void main(String[] args) throws IOException {
+        User user = new User();
         CloseableHttpClient httpClient = HttpClients.createDefault();
         HttpGet httpGet = new HttpGet("http://localhost:8080/user/json/1");
         String response = httpClient.execute(httpGet,new BasicResponseHandler());
